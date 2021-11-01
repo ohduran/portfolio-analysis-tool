90281c1df8cad85217c7437fb79ead1ae7a0fafd
ohduran <alvaro.duranb@hotmail.com>
2021-11-01 22:11:43 +0100

    postgres in actions

--- backend/backend/settings/development.py
+++ backend/backend/settings/development.py
@@ -20,7 +20,20 @@ DATABASES = {
         "NAME": "postgres",
         "USER": "postgres",
         "PASSWORD": "postgres",
-        "HOST": os.environ.get("DATABASE_HOST", "localhost"),
+        "HOST": "db",
         "PORT": 5432,
     }
 }
+
+
+if os.environ.get("GITHUB_WORKFLOW"):
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": "github_actions",
+            "USER": "postgres",
+            "PASSWORD": "postgres",
+            "HOST": "127.0.0.1",
+            "PORT": "5432",
+        }
+    }
